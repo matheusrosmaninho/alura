1c1c1e0a19d74c123cb3d84775a1b09add355be4
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-26 20:24:13 -0300

    feat: Cria migraton de inserço de cliente

---
+++ cSharp/Testes com bd/Alura.ByteBank.Dados/Migrations/20231126230823_PopularBancoByteBank.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Alura.ByteBank.Dados.Migrations
+{
+    public partial class PopularBancoByteBank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO bytebankBD.cliente (Id, CPF, Nome, Profissao, Identificador) VALUES(0, '12345678911', 'Bruno Mars', 'Cantor', UUID());");
+            migrationBuilder.Sql(
+                "INSERT INTO bytebankBD.cliente (Id, CPF, Nome, Profissao, Identificador) VALUES(0, '12345678915', 'Maria Mars', 'Cantora', UUID());");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
