402a5dbc9e9f5e6c7860ad93dfe52fca8c127cac
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-11-27 23:21:49 -0300

    Cria teste mock e stub

--- cSharp/Testes com bd/Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
+++ cSharp/Testes com bd/Alura.ByteBank.Infraestrutura.Testes/ContaCorrenteRepositorioTestes.cs
@@ -3,6 +3,7 @@ using Alura.ByteBank.Dominio.Entidades;
 using Moq;
 using System;
 using System.Collections.Generic;
+using Alura.ByteBank.Infraestrutura.Testes.DTO;
 using Xunit;
 
 namespace Alura.ByteBank.Infraestrutura.Testes;
@@ -77,7 +78,7 @@ public class ContaCorrenteRepositorioTestes
         var mock = bytebankRepositorioMock.Object;
 
         //Act
-        var lista = mock.BuscarContasCorrentes();
+        mock.BuscarContasCorrentes();
 
         //Assert - Verificando o comportamento
         bytebankRepositorioMock.Verify(b => b.BuscarContasCorrentes());
@@ -91,7 +92,7 @@ public class ContaCorrenteRepositorioTestes
         var mock = pixRepositorioMock.Object;
 
         //Act
-        var lista = mock.consultaPix(new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a"));
+        mock.consultaPix(new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a"));
 
         //Assert - Verificando o comportamento
         pixRepositorioMock.Verify(b => b.consultaPix(new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a")));
@@ -100,11 +101,11 @@ public class ContaCorrenteRepositorioTestes
     [Fact]
     public void TestaInsereUmaNovaContaCorrenteNoBanciDeDados()
     {
-        var conta = new ContaCorrente()
+        var conta = new ContaCorrente
         {
             Saldo = 10,
             Identificador = Guid.NewGuid(),
-            Cliente = new Cliente()
+            Cliente = new Cliente
             {
                 Nome = "Kent Nelson",
                 CPF = "486.074.980-45",
@@ -112,7 +113,7 @@ public class ContaCorrenteRepositorioTestes
                 Profissao = "Bancario",
                 Id = 1
             },
-            Agencia = new Agencia()
+            Agencia = new Agencia
             {
                 Nome = "Agencia central",
                 Identificador = Guid.NewGuid(),
@@ -124,5 +125,20 @@ public class ContaCorrenteRepositorioTestes
 
         var retorno = _repositorio.Adicionar(conta);
         Assert.True(retorno);
+    }
+
+    [Fact]
+    public void TestaConsultaPix()
+    {
+        var guid = new Guid("a0b80d53-c0dd-4897-ab90-c0615ad80d5a");
+        var pix = new PixDTO {Chave = guid, Saldo = 10};
+
+        var pixRepositorioMock = new Mock<IPixRepositorio>();
+        pixRepositorioMock.Setup(x => x.consultaPix(It.IsAny<Guid>())).Returns(pix);
+
+        var mock = pixRepositorioMock.Object;
+
+        var saldo = mock.consultaPix(guid).Saldo;
+        Assert.Equal(10, saldo);
     }
 }
